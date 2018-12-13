c19490604a95bd78901cf4bd9b27a05ee32eaea5
vinegr <vinegr@rambler.ru>
2018-12-13 16:26:35 +0300

    зуминг по клику, кулиски с деталями

--- js/amchart-widget.js
+++ js/amchart-widget.js
@@ -1,12 +1,13 @@
 am4core.useTheme(am4themes_kelly);
 am4core.useTheme(am4themes_animated);
 
-var zLevel = 3, cdata = _flter(od, function(v){ return v.z == 12; });
+var zLevel = 3, cdata = _flter(od, function(v){ return v.z == 10; });
+var detail, dLabel;
 var chart = am4core.createFromConfig(
 {
   "geodata": "worldHigh",
   "projection": "Miller",
-  "homeZoomLevel": 3,
+  "homeZoomLevel": 4,
   "homeGeoPoint": {
     "latitude": 60,
     "longitude": 100
@@ -14,14 +15,14 @@ var chart = am4core.createFromConfig(
   events:{
 	"zoomlevelchanged": function(ev){
 		var z = chart.zoomLevel;
-		if(zLevel >= 12 && z <12){ //zoom out
-		  cdata = _flter(od, function(v){ return v.z == 12; });
+		if(zLevel >= 8 && z <8){ //zoom out
+		  cdata = _flter(od, function(v){ return v.z == 10; });
 		  s1.data = cdata
-		  s2.data = cdata
-		}else if(zLevel <= 12 && z > 12){ 
-		  cdata = _flter(od, function(v){ return v.z < 12; });			  
+		  s2.show();
+		}else if(zLevel <= 8 && z > 8){ 
+		  s2.hide();
+		  cdata = _flter(od, function(v){ return v.z == 20; });			  
   		  s1.data = cdata
-		  s2.data = cdata
 		}
 		zLevel = z;
 	}
@@ -36,7 +37,7 @@ var chart = am4core.createFromConfig(
 
   }
   ,
-  { //картинки
+    { //картинки
     "id": "imgs",
     "type": "MapImageSeries", 	
 	"mapImages":{
@@ -50,14 +51,48 @@ var chart = am4core.createFromConfig(
 		"nonScaling": true,
 		"children":[
 			{
-				type: "Image",
-				"width": 30,
-				"height": 30,				
+				type: "Image",			
 				"propertyFields":{
-					"href": "img"
+					"href": "img",
+					"width": "size",
+					"height": "size"
 				},
 				"horizontalCenter": "middle",
-				"verticalCenter": "middle"
+				"verticalCenter": "middle",
+				"events":{
+					"over":function(ev, a, b, c){
+						var sw = document.getElementById("chartdiv").offsetWidth/2;
+						var cw = ev.event.clientX;
+						if(detail == null) {
+							detail = chart.tooltipContainer.createChild(am4core.Container);
+							detail.width = am4core.percent(45);
+							detail.height = am4core.percent(100)
+							var gradient = new am4core.LinearGradient();
+							gradient.addColor(am4core.color("#429184"));
+							gradient.addColor(am4core.color("#235642"));
+							gradient.rotation = -90;
+							detail.background.fill = gradient;
+							detail.background.fillOpacity = 0.2;
+							dLabel = detail.createChild(am4core.Image);	
+							dLabel.align = "center";
+							dLabel.valign = "top";
+							dLabel.width = am4core.percent(100);
+							dLabel.height = am4core.percent(100);
+							dLabel.href = 'images/details.gif';
+						}		
+						detail.align = (cw < sw)?"right":"left";
+						detail.show();
+					},
+					"out":function(ev){
+						detail.hide();
+					},
+					"hit": function(ev) {
+						var q = s1.data[ev.target.dataItem.index];
+						var geop = {latitude: q.lat, longitude: q.lon};
+//						geop = {"latitude": 60., "longitude": 100. }
+						chart.zoomToGeoPoint(geop, 12, true)
+					}
+				}
 			}
 		]
 	}
@@ -71,16 +106,15 @@ var chart = am4core.createFromConfig(
 			"latitude": "lat",
 			"longitude": "lon",
 		},
-		"tooltipText": "{name}",
 		"nonScaling": true,
 		"children":[
 			{
 				"type": "PieChart",
-				"width": 50,
-				"height": 50,
+				"width": 70,
+				"height": 70,
 				"horizontalCenter": "middle",
 				"verticalCenter": "middle",
-				"innerRadius": "60%",
+				"innerRadius": "30%",
 				"series": [{
 					"type": "PieSeries",
 					"dataFields": {
@@ -89,19 +123,30 @@ var chart = am4core.createFromConfig(
 					},
 					"labels":{disabled: true},
 					"ticks": {disabled: true},
-					"slices": {tooltipText: ""}
+					"slices": {
+						"events":{
+							"over":function(ev){
+								}
+						}
+					}
 				}],
 				"adapter":{
 					"data": function(data, target) {
 						return [{
-							"category": "Category #1",
+							"category": "Показатель KPI №1",
 							"value": Math.floor(Math.random() * 300)
 							}, {
-							"category": "Category #2",
+							"category": "Показатель KPI №2",
 							"value": Math.floor(Math.random() * 300) + 100
 							}, {
-							"category": "Category #3",
+							"category": "Показатель KPI №3",
 							"value": Math.floor(Math.random() * 300) + 200
+							}, {
+							"category": "Показатель KPI №4",
+							"value": Math.floor(Math.random() * 500) + 100
+							}, {
+							"category": "Показатель KPI №5",
+							"value": Math.floor(Math.random() * 500) 
 							}];  
 					}
 				}
--- js/oilfields.js
+++ js/oilfields.js
@@ -1,28 +1,33 @@
 
 var od = [
-{t:"field", lat:61.170210, lon:76.735759, name:"Самотлорское месторождение","z": 12}
-,{t:"field", lat:63.228871, lon:70.630984, name:"Приобское месторождение","z": 12}
-,{t:"field", lat:59.773989, lon:71.750712, name:"Салымское месторождение", "z": 12}
-,{t:"field", lat:60.770970, lon:72.823433, name:"Мамонтовское месторождение", "z": 12}
-,{t:"field", lat:50.880739, lon:142.467392, name:"Сахалин-5 месторождение", "z": 12}
-,{t:"field", lat:44.456643, lon:50.655297, name:"Курмангазы месторождение", "z": 12}
-,{t:"field", lat:60.310998, lon:69.0, name:"Приразломное месторождение", "z": 12}
-,{t:"field", lat:67.806708, lon:83.552957, name:"Ванкорское месторождение", "z": 12}
-,{t:"field", lat:60.432888, lon:96.249800, name:"Юрубчено тохомское месторождение", "z": 12}
-,{t:"field", lat:60.635639, lon:107.851504, name:"Савостьяновское месторождение", "z": 12}
-,{t:"field", lat:61.215436, lon:72.826109, name:"Усть-Балыкское месторождение", "z": 12}
+{s:"", t:"field", lat:61.170210, lon:76.735759, name:"Самотлорское <br> месторождение","z": 10, size:20}
+,{s:"", t:"field", lat:63.228871, lon:70.630984, name:"Приобское <br> месторождение","z": 10, size:20}
+,{s:"", t:"field", lat:56.773989, lon:71.950712, name:"Салымское <br> месторождение", "z": 10, size:20}
+//,{t:"field", lat:60.770970, lon:72.823433, name:"Мамонтовское <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:50.880739, lon:142.467392, name:"Сахалин-5 <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:44.456643, lon:50.655297, name:"Курмангазы <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:60.310998, lon:68.5, name:"Приразломное <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:67.806708, lon:83.552957, name:"Ванкорское <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:60.432888, lon:96.249800, name:"Юрубчено тохомское <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:60.635639, lon:107.851504, name:"Савостьяновское <br> месторождение", "z": 10, size:20}
+,{s:"", t:"field", lat:61.215436, lon:72.826109, name:"Усть-Балыкское <br> месторождение", "z": 10, size:20}
 ]
 
 var derricks = [], item ={};
 for(var i=0; i<od.length; i++){
-	var q = Math.floor(Math.random()*7 + 2);
+	var q = Math.floor(Math.random()*6 + 8);
 	Object.assign(item,od[i]);
 	while(q-- > 0){		
-		item.t = "derrick";
-		item.name = "Скважина №"+q+""+i;
-		item.lon = item.lon + Math.random()*3.5 - 1.5;
-		item.lat = item.lat + Math.random()*3.5 - 1.5;
-		item.z = 3;
+		item.t = (Math.random() > 0.66)? "derrick" : "tower";
+		item.name = ((item.t == "derrick")?"Участок №":"Буровая №")+q+""+i;
+		item.lon = item.lon + Math.random()*2. - 1.05;
+		item.lat = item.lat + Math.random()*2.5 - 1.25;
+		item.z = 20;
+		sq = Math.random()*100.;
+		item.s= (sq<5)? "off":
+                (sq>95)? "alert":    		
+				(sq<15)? "warn": "norm";
+		item.size =	30;	
 		var it = {};
 		Object.assign(it,item);
 		derricks.push(it);
@@ -32,7 +37,7 @@ for(var i=0; i<od.length; i++){
 od = od.concat(derricks);
  
 od = _each(od, function(v){
-	v.img="images/"+v.t+".jpg";
+	v.img="images/"+v.s+v.t+".gif";
 });
 
 
